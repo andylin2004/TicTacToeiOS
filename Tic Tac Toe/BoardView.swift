f789076a0a3a9d1f33ccdc8f5156bf404e25f3c3
Andy <alin21@stuy.edu>
2021-02-13 22:15:31 -0500

    init commit

---
+++ Tic Tac Toe/BoardView.swift
@@ -0,0 +1,22 @@
+//
+//  BoardView.swift
+//  Tic Tac Toe
+//
+//  Created by Andy Lin on 2/13/21.
+//
+
+import SwiftUI
+
+struct BoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
+    }
+}
+
+struct BoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoardView()
+    }
+}
